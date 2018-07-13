21133a3f5ede2760b6100ca2d8db9b467e78067b
lsl <songlin.lin@yundata.com>
2018-07-13 22:10:58 +0800

    增加wxSearch

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,5 +1,8 @@
 var wemark = require('wemark/wemark');
+var WxSearch = require('../../wxSearch/wxSearch.js')
+
 const app = getApp()
+
 
 var md = '# hello, world';
 
@@ -7,13 +10,18 @@ Page({
   data: {
     query: null,
   },
-  onLoad: function() {},
+  onLoad: function() {
+    var that = this
+    //初始化的时候渲染wxSearchdata 第二个为你的search高度
+    WxSearch.init(that, 43, ['python flask', 'python django', 'python scrapy', 'python requests']);
+    WxSearch.initMindKeys(['python flask', 'python django', 'python scrapy', 'python requests']);
+  },
   onReady: function() {
     wemark.parse(md, this, {
       name: 'wemark'
     })
   },
-  inputQuery: function (e) {
+  inputQuery: function(e) {
     this.setData({
       query: e.detail.value
     })
@@ -24,9 +32,9 @@ Page({
     wx.request({
       url: 'https://www.linsl2018.top:5023/hello',
       data: {
-        query:this.data.query
+        query:that.data.wxSearchData.value
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res.data)
         var md = res.data.result
         wemark.parse(md, that, {
@@ -34,5 +42,39 @@ Page({
         })
       }
     })
+  },
+  wxSearchFn: function(e) {
+    var that = this
+    WxSearch.wxSearchAddHisKey(that);
+    this.query()
+
+  },
+  wxSearchInput: function(e) {
+    var that = this
+    WxSearch.wxSearchInput(e, that);
+  },
+  wxSerchFocus: function(e) {
+    var that = this
+    WxSearch.wxSearchFocus(e, that);
+  },
+  wxSearchBlur: function(e) {
+    var that = this
+    WxSearch.wxSearchBlur(e, that);
+  },
+  wxSearchKeyTap: function(e) {
+    var that = this
+    WxSearch.wxSearchKeyTap(e, that);
+  },
+  wxSearchDeleteKey: function(e) {
+    var that = this
+    WxSearch.wxSearchDeleteKey(e, that);
+  },
+  wxSearchDeleteAll: function(e) {
+    var that = this;
+    WxSearch.wxSearchDeleteAll(that);
+  },
+  wxSearchTap: function(e) {
+    var that = this
+    WxSearch.wxSearchHiddenPancel(that);
   }
 })
